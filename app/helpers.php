8ac6f60a225a9064483bee614d82cf87b140c2e0
David VanScott <david.vanscott@gmail.com>
2017-04-01 13:09:47 -0400

    Add some helpers

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,19 @@ if (! function_exists('alias')) {
 		return config("app.aliases.{$key}");
 	}
 }
+
+if (! function_exists('d')) {
+	function d()
+	{
+		array_map(function ($x) {
+			(new Illuminate\Support\Debug\Dumper)->dump($x);
+		}, func_get_args());
+	}
+}
+
+if (! function_exists('partial')) {
+	function partial($file, array $data = [])
+	{
+		return view("partials.{$file}", $data)->render();
+	}
+}
