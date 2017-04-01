e35b594544d575e5096fdc986cc85192e133497a
David VanScott <david.vanscott@gmail.com>
2017-04-01 13:10:53 -0400

    Test updates

--- tests/Feature/CreateDiscussionsTest.php
+++ tests/Feature/CreateDiscussionsTest.php
@@ -4,31 +4,31 @@ use Tests\DatabaseTestCase;
 
 class CreateDiscussionsTest extends DatabaseTestCase
 {
-    /** @test **/
+	/** @test **/
 	public function a_guest_cannot_start_new_discussions()
 	{
-        $this->withExceptionHandling();
+		$this->withExceptionHandling();
 
-        $this->get(route('discussions.create'))
-            ->assertRedirect('/login');
+		$this->get(route('discussions.create'))
+			->assertRedirect('/login');
 
-        $this->post(route('discussions.store'))
-            ->assertRedirect('/login');
+		$this->post(route('discussions.store'))
+			->assertRedirect('/login');
 	}
 
-    /** @test **/
-    public function an_authenticated_user_can_start_new_discussions()
-    {
-        $this->signIn();
+	/** @test **/
+	public function an_authenticated_user_can_start_new_discussions()
+	{
+		$this->signIn();
 
-        $discussion = make('App\Data\Discussion');
+		$discussion = make('App\Data\Discussion');
 
-        $response = $this->post(route('discussions.store'), $discussion->toArray());
+		$response = $this->post(route('discussions.store'), $discussion->toArray());
 
 		$this->get($response->headers->get('Location'))
 			->assertSee($discussion->title)
 			->assertSee($discussion->body);
-    }
+	}
 
 	/** @test **/
 	public function an_authenticated_user_can_view_the_create_discussions_page()
@@ -39,41 +39,41 @@ class CreateDiscussionsTest extends DatabaseTestCase
 			->assertSee('Start a New Discussion');
 	}
 
-    /** @test **/
-    public function a_discussion_requires_a_title()
-    {
-        $this->publishDiscussion(['title' => null])
-            ->assertSessionHasErrors('title');
-    }
+	/** @test **/
+	public function a_discussion_requires_a_title()
+	{
+		$this->publishDiscussion(['title' => null])
+			->assertSessionHasErrors('title');
+	}
 
-    /** @test **/
-    public function a_discussion_requires_a_body()
-    {
-        $this->publishDiscussion(['body' => null])
-            ->assertSessionHasErrors('body');
-    }
+	/** @test **/
+	public function a_discussion_requires_a_body()
+	{
+		$this->publishDiscussion(['body' => null])
+			->assertSessionHasErrors('body');
+	}
 
-    /** @test **/
-    public function a_discussion_requires_a_valid_topic_id()
-    {
-        factory('App\Data\Topic', 2)->create();
+	/** @test **/
+	public function a_discussion_requires_a_valid_topic_id()
+	{
+		factory('App\Data\Topic', 2)->create();
 
-        $this->publishDiscussion(['topic_id' => null])
-            ->assertSessionHasErrors('topic_id');
+		$this->publishDiscussion(['topic_id' => null])
+			->assertSessionHasErrors('topic_id');
 
-        $this->publishDiscussion(['topic_id' => 999])
-            ->assertSessionHasErrors('topic_id');
+		$this->publishDiscussion(['topic_id' => 999])
+			->assertSessionHasErrors('topic_id');
 
-        $this->publishDiscussion(['topic_id' => 'foo'])
-            ->assertSessionHasErrors('topic_id');
-    }
+		$this->publishDiscussion(['topic_id' => 'foo'])
+			->assertSessionHasErrors('topic_id');
+	}
 
-    protected function publishDiscussion(array $overrides = [])
-    {
-        $this->withExceptionHandling()->signIn();
+	protected function publishDiscussion(array $overrides = [])
+	{
+		$this->withExceptionHandling()->signIn();
 
-        $discussion = make('App\Data\Discussion', $overrides);
+		$discussion = make('App\Data\Discussion', $overrides);
 
-        return $this->post(route('discussions.store'), $discussion->toArray());
-    }
+		return $this->post(route('discussions.store'), $discussion->toArray());
+	}
 }
--- tests/Feature/ReadDiscussionsTest.php
+++ tests/Feature/ReadDiscussionsTest.php
@@ -4,38 +4,38 @@ use Tests\DatabaseTestCase;
 
 class ReadDiscussionsTest extends DatabaseTestCase
 {
-    protected $discussion;
+	protected $discussion;
 
-    public function setUp()
-    {
-        parent::setUp();
+	public function setUp()
+	{
+		parent::setUp();
 
-        $this->discussion = create('App\Data\Discussion');
-    }
+		$this->discussion = create('App\Data\Discussion');
+	}
 
-    /** @test **/
-    public function a_user_can_view_all_discussions()
-    {
-        $this->get(route('home'))->assertSee($this->discussion->title);
-    }
+	/** @test **/
+	public function a_user_can_view_all_discussions()
+	{
+		$this->get(route('home'))->assertSee($this->discussion->title);
+	}
 
-    /** @test **/
-    public function a_user_can_view_a_single_discussion()
-    {
-        $this->get(route('discussions.show', [$this->discussion->topic->slug, $this->discussion]))
-            ->assertSee($this->discussion->title);
-    }
+	/** @test **/
+	public function a_user_can_view_a_single_discussion()
+	{
+		$this->get(route('discussions.show', [$this->discussion->topic->slug, $this->discussion]))
+			->assertSee($this->discussion->title);
+	}
 
-    /** @test **/
-    public function a_user_can_view_replies_associated_with_a_discussion()
-    {
-        $reply = create('App\Data\Reply', [
-            'discussion_id' => $this->discussion->id
-        ]);
+	/** @test **/
+	public function a_user_can_view_replies_associated_with_a_discussion()
+	{
+		$reply = create('App\Data\Reply', [
+			'discussion_id' => $this->discussion->id
+		]);
 
-        $this->get(route('discussions.show', [$this->discussion->topic->slug, $this->discussion]))
-            ->assertSee($reply->body);
-    }
+		$this->get(route('discussions.show', [$this->discussion->topic->slug, $this->discussion]))
+			->assertSee($reply->body);
+	}
 
 	/** @test **/
 	public function a_user_can_filter_discussions_according_to_a_topic()
@@ -47,5 +47,22 @@ class ReadDiscussionsTest extends DatabaseTestCase
 		$this->get(route('topics.discussions', [$topic]))
 			->assertSee($discussionInTopic->title)
 			->assertDontSee($discussionNotInTopic->title);
+	}
+
+	/** @test **/
+	public function a_user_can_filter_discussions_by_any_username()
+	{
+		$user = create('App\Data\User');
+
+		$this->signIn($user);
+
+		$discussionByJohn = create('App\Data\Discussion', ['user_id' => $user->id]);
+		$discussionNotByJohn = create('App\Data\Discussion');
+
+		//d($user->id, $discussionByJohn->toArray(), $discussionNotByJohn->toArray());
+
+		$this->get(route('home').'?by='.$user->name)
+			->assertSee($discussionByJohn->title)
+			->assertDontSee($discussionNotByJohn->title);
 	}
 }
