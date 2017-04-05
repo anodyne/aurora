5628a23f4ad10add3920596ad6350ead6776c996
David VanScott <david.vanscott@gmail.com>
2017-04-05 16:01:07 -0400

    Add a filter for popular discussions

--- app/Filters/DiscussionFilters.php
+++ app/Filters/DiscussionFilters.php
@@ -4,12 +4,20 @@ use App\Data\User;
 
 class DiscussionFilters extends Filters
 {
-	protected $filters = ['by'];
+	protected $filters = ['by', 'popular'];
 
 	protected function by($username)
 	{
 		$user = User::where('name', $username)->firstOrFail();
 		
 		return $this->builder->where('user_id', $user->id);
+	}
+
+	protected function popular()
+	{
+		// Clear out any existing ordering that we have
+		$this->builder->getQuery()->orders = [];
+
+		$this->builder->orderBy('replies_count', 'desc');
 	}
 }
