dcc6e83a112a8d35685544c910cc95c4f67b258d
David VanScott <david.vanscott@gmail.com>
2017-04-05 16:03:44 -0400

    Discussion show work

--- resources/views/pages/discussions/show.blade.php
+++ resources/views/pages/discussions/show.blade.php
@@ -3,32 +3,50 @@
 @section('content')
 	<h1>{{ $discussion->title }}</h1>
 
-	<ul class="breadcrumb">
-		<li><a href="{{ route('home') }}">Forum</a></li>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Forum</a></li>
+	
+		@if ($topic->parent_id > 0)
+			<li class="breadcrumb-item"><a href="{{ route('topics.discussions', [$topic->parent]) }}">{{ $topic->parent->name }}</a></li>
+		@endif
+		
+		<li class="breadcrumb-item active"><a href="{{ route('topics.discussions', [$topic]) }}">{{ $topic->name }}</a></li>
+	</ol>
 
-		<li class="active"><a href="{{ route('topics.discussions', [$discussion->topic]) }}">{{ $discussion->topic->name }}</a></li>
-	</ul>
+	<div class="row">
+		<div class="col-md-8">
+			{!! view('pages.discussions._post')->with('post', $discussion) !!}
 
-	{!! view('pages.discussions._post')->with('post', $discussion) !!}
+			@each('pages.discussions._post', $replies, 'post')
 
-	@each('pages.discussions._post', $discussion->replies, 'post')
+			{{ $replies->links() }}
 
-	@if (auth()->check())
-		<hr>
+			@if (auth()->check())
+				<hr>
 
-		<div class="media">
-			<div class="media-left"><a href="#" class="avatar sm" style="background-image:url(http://placehold.it/100)"></a></div>
-			<div class="media-body">
-				{!! Form::open(['route' => ['discussions.replies', $topic, $discussion]]) !!}
-					<div class="form-group">
-						{!! Form::textarea('body', null, ['class' => 'form-control', 'rows' => 5, 'placeholder' => 'Reply to this discussion now...']) !!}
+				<div class="media">
+					<div class="media-left">{!! $_user->present()->avatar(['type' => 'image', 'class' => 'avatar sm']) !!}</div>
+					<div class="media-body">
+						{!! Form::open(['route' => ['discussions.replies', $topic, $discussion]]) !!}
+							<div class="form-group">
+								{!! Form::textarea('body', null, ['class' => 'form-control', 'rows' => 5, 'placeholder' => 'Reply to this discussion now...']) !!}
+							</div>
+
+							<div class="form-group">
+								{!! Form::button('Reply', ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
+							</div>
+						{!! Form::close() !!}
 					</div>
+				</div>
+			@endif
+		</div>
 
-					<div class="form-group">
-						{!! Form::button('Reply', ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
-					</div>
-				{!! Form::close() !!}
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-block">
+					This discussion was published {{ $discussion->present()->createdDiff }} by {{ $discussion->present()->author }} and has {{ $discussion->replies_count }} {{ str_plural('reply', $discussion->replies_count) }}.
+				</div>
 			</div>
 		</div>
-	@endif
+	</div>
 @endsection
