a0151dc66d329ee40dfbf45df4d3e57751000b88
David VanScott <david.vanscott@gmail.com>
2017-04-05 16:03:58 -0400

    Re-factor the discussion list

--- resources/views/partials/discussions-list.blade.php
+++ resources/views/partials/discussions-list.blade.php
@@ -1,27 +1,26 @@
 <div class="data-list">
 @foreach ($discussions as $discussion)
-	<div class="row">
-		<div class="col-10">
-			<div class="list-item-avatar">
-				<span class="hidden-sm-up">{!! $discussion->present()->authorAvatar(true, 'xs') !!}</span>
-				<span class="hidden-xs-down">{!! $discussion->present()->authorAvatar !!}</span>
-				@if ($discussion->answer)
-					<span class="list-item-answered" title="Discussion has a reply marked as a correct answer">@icon('check')</span>
-				@endif
+	<div class="d-flex align-items-start align-items-md-center justify-content-start mb-2 p-2">
+		<div class="d-flex align-items-start align-items-md-center mr-auto">
+			<div class="mr-3">
+				<div class="hidden-sm-down">{!! $discussion->present()->authorAvatar !!}</div>
+				<div class="hidden-md-up pt-2">{!! $discussion->present()->authorAvatar(true, 'xs') !!}</div>
 			</div>
-
-			<div class="list-item-group justify-content-between">
+			<div>
 				{!! $discussion->present()->titleAsLink !!}
-
 				{!! $discussion->present()->topic !!}
-				<span class="list-item-meta">
+				<span class="small text-muted pl-2">
 					{!! $discussion->present()->updatedAt !!}
 					{!! $discussion->present()->updatedBy !!}
 				</span>
 			</div>
 		</div>
-		<div class="col-2">
-			{!! $discussion->present()->replyCount !!}
+		<div class="text-center pt-1">
+			<div class="font-special mb-0">
+				<span class="hidden-sm-down h3 font-weight-3">{{ $discussion->replies_count }}</span>
+				<span class="hidden-md-up h4 font-weight-4 ">{{ $discussion->replies_count }}</span>
+			</div>
+			<small>{{ str_plural('reply', $discussion->replies_count) }}</small>
 		</div>
 	</div>
 @endforeach
