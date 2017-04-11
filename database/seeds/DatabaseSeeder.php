249a02256a0157c14f24b678eda809b26afa62b6
David VanScott <david.vanscott@gmail.com>
2017-04-10 21:18:45 -0400

    Working on seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,8 +4,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
-    {
-        $this->call(ForumSeeder::class);
-    }
+	public function run()
+	{
+		$this->callDevelopmentSeeders();
+
+		$this->callProductionSeeders();
+	}
+
+	protected function callDevelopmentSeeders()
+	{
+		if ($this->container->environment() != 'production') {
+			$this->call(ForumDevelopmentSeeder::class);
+		}
+	}
+
+	protected function callProductionSeeders()
+	{
+		if ($this->container->environment() == 'production') {
+			$this->call(ForumProductionSeeder::class);
+		}
+	}
 }
--- database/seeds/ForumDevelopmentSeeder.php
+++ database/seeds/ForumDevelopmentSeeder.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Database\Seeder;
 
-class ForumSeeder extends Seeder
+class ForumDevelopmentSeeder extends Seeder
 {
-    public function run()
-    {
+	public function run()
+	{
 		$faker = Faker\Factory::create();
 
-        for ($t = 1; $t <= 10; $t++) {
-            factory('App\Data\Topic')->create();
-        }
+		for ($t = 1; $t <= 10; $t++) {
+			factory('App\Data\Topic')->create();
+		}
 
 		for ($d = 1; $d <= 50; $d++) {
 			$discussion = factory('App\Data\Discussion')->create([
 				'user_id' => $faker->numberBetween(1, 100),
-                'topic_id' => $faker->numberBetween(1, 10),
+				'topic_id' => $faker->numberBetween(1, 10),
 			]);
 
 			for ($r = 0; $r <= $faker->numberBetween(0, 10); $r++) {
@@ -25,5 +25,5 @@ class ForumSeeder extends Seeder
 				]);
 			}
 		}
-    }
+	}
 }
