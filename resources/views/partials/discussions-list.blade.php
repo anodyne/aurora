fcbb24bfc1070cd2f7ef9cfc56d8ce0f1438d04e
David VanScott <david.vanscott@gmail.com>
2017-04-11 17:05:48 -0400

    Big updates to discussion lists

--- resources/views/partials/discussions-list.blade.php
+++ resources/views/partials/discussions-list.blade.php
@@ -1,32 +1,42 @@
 <div class="data-list">
 @foreach ($discussions as $discussion)
-	<div class="d-flex align-items-start align-items-md-center justify-content-start mb-2 p-2">
-		<div class="d-flex align-items-start align-items-md-center mr-auto">
-			<div class="mr-3">
-				<div class="hidden-sm-down">
-					{!! avatar($discussion->author)->link() !!}
-					@if ($discussion->answer)
-						<span class="answered" title="Discussion has a reply marked as a correct answer">@icon('check')</span>
-					@endif
-				</div>
-				<div class="hidden-md-up pt-2">
-					{!! avatar($discussion->author)->link()->small() !!}
-					@if ($discussion->answer)
-						<span class="list-item-answered" title="Discussion has a reply marked as a correct answer">@icon('check')</span>
-					@endif
-				</div>
+	<div class="block">
+		<div class="item">
+			<div class="item-avatar">
+				{!! avatar($discussion->author)->link() !!}
 			</div>
-			<div>
+
+			<div class="item-content">
 				{!! $discussion->present()->titleAsLink !!}
-				{!! $discussion->present()->topic !!}
-				<span class="small text-muted pl-2">
-					{!! $discussion->present()->updatedAt !!}
-					{!! $discussion->present()->updatedBy !!}
-				</span>
+
+				<div class="meta">
+					<div class="meta-item topic">
+						{!! $discussion->present()->topic !!}
+					</div>
+
+					@if ($discussion->replies_count > 0)
+						<div class="meta-item replies">
+							@icon('chat')
+							<span>{{ $discussion->replies_count }} {{ str_plural('reply', $discussion->replies_count) }}</span>
+						</div>
+					@endif
+
+					<div class="meta-item timestamp">
+						@icon('clock')
+						<span>
+							{!! $discussion->present()->updatedAtRelative !!}
+							by&nbsp;{!! $discussion->present()->updatedBy !!}
+						</span>
+					</div>
+
+					@if ($discussion->answer)
+						<div class="meta-item answered">
+							@icon('check')
+							<span>answered</span>
+						</div>
+					@endif
+				</div>
 			</div>
-		</div>
-		<div class="text-center text-subtle font-special mb-0 px-2 h5 font-weight-3">
-			{{ $discussion->present()->replyCount }}
 		</div>
 	</div>
 @endforeach
