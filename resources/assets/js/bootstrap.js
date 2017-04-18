418a474983c7493df57b760d729a7a4c880739e2
David VanScott <david.vanscott@gmail.com>
2017-04-18 07:45:53 -0400

    Compile the assets

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -33,8 +33,6 @@ window.axios.defaults.headers.common = {
 	'X-Requested-With': 'XMLHttpRequest'
 }
 
-window.Quill = require('quill')
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
