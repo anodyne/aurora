10d92b22cf71be6a9367673ca1cc29b8b8099936
David VanScott <david.vanscott@gmail.com>
2017-04-18 22:36:03 -0400

    Updates to the seeders

--- database/seeds/ForumDevelopmentSeeder.php
+++ database/seeds/ForumDevelopmentSeeder.php
@@ -31,22 +31,22 @@ class ForumDevelopmentSeeder extends Seeder
 	protected function seedTopics()
 	{
 		$data = [
-			['name' => "Nova", 'slug' => "nova-general", 'color' => "#259b24", 'description' => ""],
-			['name' => "Anodyne Lounge", 'slug' => "anodyne-lounge", 'color' => "#607d8b", 'description' => ""],
-			['name' => "Announcements", 'slug' => "announcements", 'color' => "#607d8b", 'description' => ""],
-			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60", 'description' => ""],
-			['name' => "Help Center", 'slug' => "help-center", 'color' => "#0288d1", 'description' => ""],
+			['name' => "Nova", 'slug' => "nova-general", 'color' => "#259b24"],
+			['name' => "Anodyne Lounge", 'slug' => "anodyne-lounge", 'color' => "#607d8b"],
+			['name' => "Announcements", 'slug' => "announcements", 'color' => "#607d8b"],
+			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60"],
+			['name' => "Help Center", 'slug' => "help-center", 'color' => "#0288d1"],
 			
 			// Nova sub topics
-			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
-			['name' => "Themeing", 'slug' => "nova-themeing", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
-			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
+			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'parent_id' => 1],
+			['name' => "Themeing", 'slug' => "nova-themeing", 'color' => "#259b24", 'parent_id' => 1],
+			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'parent_id' => 1],
 
 			// AnodyneXtras sub topics
-			['name' => "Xtras Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'description' => "", 'parent_id' => 4],
+			['name' => "Xtras Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'parent_id' => 4],
 
 			// Help Center sub topics
-			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'description' => "", 'parent_id' => 5],
+			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'parent_id' => 5],
 		];
 
 		foreach ($data as $d)
--- database/seeds/ForumProductionSeeder.php
+++ database/seeds/ForumProductionSeeder.php
@@ -7,24 +7,24 @@ class ForumProductionSeeder extends Seeder
 	public function run()
 	{
 		$data = [
-			['name' => "Nova", 'slug' => "nova-general", 'color' => "#259b24", 'description' => ""],
-			['name' => "Anodyne Lounge", 'slug' => "", 'color' => "#607d8b", 'description' => ""],
-			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60", 'description' => ""],
-			['name' => "Help Center", 'slug' => "", 'color' => "#0288d1", 'description' => ""],
+			['name' => "Nova", 'slug' => "nova-general", 'color' => "#259b24"],
+			['name' => "Anodyne Lounge", 'slug' => "", 'color' => "#607d8b"],
+			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60"],
+			['name' => "Help Center", 'slug' => "", 'color' => "#0288d1"],
 			
 			// Nova sub topics
-			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
-			['name' => "Themeing", 'slug' => "nova-themeing", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
-			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
+			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'parent_id' => 1],
+			['name' => "Themeing", 'slug' => "nova-themeing", 'color' => "#259b24", 'parent_id' => 1],
+			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'parent_id' => 1],
 
 			// Anodyne Lounge sub topics
-			['name' => "Announcements", 'slug' => "", 'color' => "#607d8b", 'description' => "", 'parent_id' => 2],
+			['name' => "Announcements", 'slug' => "", 'color' => "#607d8b", 'parent_id' => 2],
 
 			// AnodyneXtras sub topics
-			['name' => "Xtra Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'description' => "", 'parent_id' => 3],
+			['name' => "Xtra Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'parent_id' => 3],
 
 			// Help Center sub topics
-			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'description' => "", 'parent_id' => 4],
+			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'parent_id' => 4],
 		];
 
 		foreach ($data as $d)
