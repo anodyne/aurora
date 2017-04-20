5f341eaba2fba2b2a653a6c31cfb909514529910
David VanScott <david.vanscott@gmail.com>
2017-04-19 23:09:12 -0400

    Update the production seeds

--- database/seeds/ForumProductionSeeder.php
+++ database/seeds/ForumProductionSeeder.php
@@ -8,9 +8,9 @@ class ForumProductionSeeder extends Seeder
 	{
 		$data = [
 			['name' => "Nova", 'slug' => "nova-general", 'color' => "#259b24"],
-			['name' => "Anodyne Lounge", 'slug' => "", 'color' => "#607d8b"],
+			['name' => "Anodyne Lounge", 'slug' => "anodyne-lounge", 'color' => "#607d8b"],
 			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60"],
-			['name' => "Help Center", 'slug' => "", 'color' => "#0288d1"],
+			['name' => "Help Center", 'slug' => "help-center", 'color' => "#0288d1"],
 			
 			// Nova sub topics
 			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'parent_id' => 1],
@@ -18,7 +18,7 @@ class ForumProductionSeeder extends Seeder
 			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'parent_id' => 1],
 
 			// Anodyne Lounge sub topics
-			['name' => "Announcements", 'slug' => "", 'color' => "#607d8b", 'parent_id' => 2],
+			['name' => "Announcements", 'slug' => "announcements", 'color' => "#607d8b", 'parent_id' => 2],
 
 			// AnodyneXtras sub topics
 			['name' => "Xtra Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'parent_id' => 3],
