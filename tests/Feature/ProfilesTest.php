795b284207f527ca413c5be4c0037c2007242128
David VanScott <david.vanscott@gmail.com>
2017-04-22 09:53:50 -0400

    Work on the profiles tests

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -7,7 +7,7 @@ class ProfilesTest extends DatabaseTestCase
 	/** @test **/
 	public function a_user_has_a_profile()
 	{
-		$user = create('App\Data\User');
+		$user = $this->createUser();
 
 		$this->get(route('profile', $user))
 			->assertSee($user->name);
@@ -16,7 +16,7 @@ class ProfilesTest extends DatabaseTestCase
 	/** @test **/
 	public function profiles_display_all_discussions_by_the_user()
 	{
-		$user = create('App\Data\User');
+		$user = $this->createUser();
 
 		$discussion = create('App\Data\Discussion', ['user_id' => $user->id]);
 
