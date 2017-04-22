7ab0fc0901479a8d10132e63465558ccddfe88bc
David VanScott <david.vanscott@gmail.com>
2017-04-22 09:53:44 -0400

    Work on the topics tests

--- tests/Feature/ManageTopicsTest.php
+++ tests/Feature/ManageTopicsTest.php
@@ -6,34 +6,88 @@ use Tests\DatabaseTestCase;
 class ManageTopicsTest extends DatabaseTestCase
 {
 	/** @test **/
-	public function a_guest_cannot_add_topics()
+	public function an_unauthorized_user_cannot_manage_topics()
 	{
 		$this->withExceptionHandling();
 
+		$topic = create('App\Data\Topic');
+
 		$this->get(route('topics.index'))
-			->assertRedirect('/login');
+			->assertRedirect(route('login'));
+
+		$this->get(route('topics.create'))
+			->assertRedirect(route('login'));
 
 		$this->post(route('topics.store'))
-			->assertRedirect('/login');
+			->assertRedirect(route('login'));
+
+		$this->get(route('topics.edit', $topic))
+			->assertRedirect(route('login'));
+
+		$this->put(route('topics.update', $topic))
+			->assertRedirect(route('login'));
+
+		$this->signIn();
+
+		$this->get(route('topics.index'))
+			->assertStatus(403);
+
+		$this->get(route('topics.create'))
+			->assertStatus(403);
+
+		$this->post(route('topics.store'))
+			->assertStatus(403);
+
+		$this->get(route('topics.edit', $topic))
+			->assertStatus(403);
+
+		$this->put(route('topics.update', $topic))
+			->assertStatus(403);
 	}
 
-	// TODO: an_authorized_user_can_add_topics
-	// TODO: an_unauthorized_user_cannot_add_topics
-
 	/** @test **/
-	public function an_authenticated_user_can_add_topics()
+	public function an_authorized_user_can_create_a_topic()
 	{
-		$user = create('App\Data\User');
-		$user->attachRole(Role::find(10));
+		$userAdmin = $this->createAdmin();
+		$userModerator = $this->createModerator();
 
-		$this->signIn($user);
+		$this->signIn($userAdmin);
 
 		$topic = make('App\Data\Topic');
 
-		$response = $this->post(route('topics.store'), $topic->toArray());
+		$this->get(route('topics.index'))->assertSee('Topics');
+		$this->get(route('topics.create'))->assertSee('Add Topic');
 
-		$this->get($response->headers->get('Location'))
-			->assertSee($topic->name);
+		$response = $this->post(route('topics.store'), $topic->toArray());
+		$this->get($response->headers->get('Location'))->assertSee($topic->name);
+
+		$this->signIn($userModerator);
+
+		$topic = make('App\Data\Topic');
+
+		$this->get(route('topics.index'))->assertSee('Topics');
+		$this->get(route('topics.create'))->assertSee('Add Topic');
+
+		$response = $this->post(route('topics.store'), $topic->toArray());
+		$this->get($response->headers->get('Location'))->assertSee($topic->name);
+	}
+
+	/** @test **/
+	public function an_authorized_user_can_update_a_topic()
+	{
+		// TODO
+	}
+
+	/** @test **/
+	public function an_authorized_user_can_delete_a_topic()
+	{
+		// TODO
+	}
+
+	/** @test **/
+	public function discussions_are_reassigned_to_a_new_topic_when_a_topic_is_deleted()
+	{
+		// TODO
 	}
 
 	/** @test **/
