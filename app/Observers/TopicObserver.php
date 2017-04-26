fd1df7c4f60541db37ae121dd83efe96bbb0f3cf
David VanScott <david.vanscott@gmail.com>
2017-04-26 12:50:48 -0400

    Refresh the topics cache when items are deleted

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -5,7 +5,17 @@ use App\Data\Topic;
 
 class TopicObserver extends Observer
 {
+	public function deleted(Topic $topic)
+	{
+		$this->refreshCache();
+	}
+
 	public function saved(Topic $topic)
+	{
+		$this->refreshCache();
+	}
+
+	protected function refreshCache()
 	{
 		// Clear out the old cache item
 		Cache::forget('topics');
