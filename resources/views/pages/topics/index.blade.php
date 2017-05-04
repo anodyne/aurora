d788280d3e199ea9d40bad3280b5c2d1991c02ea
David VanScott <david.vanscott@gmail.com>
2017-05-04 08:21:27 -0400

    Show trashed topics

--- resources/views/pages/topics/index.blade.php
+++ resources/views/pages/topics/index.blade.php
@@ -37,7 +37,7 @@
 			</div>
 
 			@if ($topic->has('children'))
-				@foreach ($topic->children as $child)
+				@foreach ($topic->children()->withTrashed()->get() as $child)
 					<div class="row d-flex align-items-center">
 						<div class="col-md-9 d-flex align-items-center">
 							<span class="badge badge-pill ml-4" style="background-color:{{ $child->color }}">&nbsp;</span>
@@ -48,15 +48,16 @@
 								<div class="btn-group">
 									<a href="{{ route('topics.edit', [$child]) }}" class="btn btn-secondary">@icon('edit')</a>
 								</div>
-								<div class="btn-group ml-2">
-									@if ($child->trashed())
-										{!! Form::model($child, ['route' => ['topics.restore', $child], 'method' => 'put']) !!}
-											<button type="submit" class="btn btn-outline-success">@icon('back-in-time')</button>
-										{!! Form::close() !!}
-									@else
+
+								@if ($child->trashed())
+									<div class="btn-group ml-2">
+										<a href="#" class="btn btn-outline-success js-restore" data-topic="{{ $child->slug }}">@icon('back-in-time')</a>
+									</div>
+								@else
+									<div class="btn-group ml-2">
 										<a href="#" class="btn btn-outline-danger js-remove">@icon('trash')</a>
-									@endif
-								</div>
+									</div>
+								@endif
 							</div>
 						</div>
 					</div>
@@ -70,4 +71,53 @@
 	@can('delete', Topic::class)
 		{!! modal(['id' => "removeTopic", 'header' => 'Remove Topic']) !!}
 	@endcan
+@endsection
+
+@section('js')
+	<script>
+		$('.js-restore').click(function (e) {
+			e.preventDefault()
+
+			var topic = $(this).data('topic')
+
+			swal({   
+				title: "Restore this topic?",
+				type: "question",
+				showCancelButton: true,
+				confirmButtonClass: 'btn btn-success mr-2',
+				confirmButtonText: "Yes, restore it!",
+				cancelButtonClass: 'btn btn-outline-secondary ml-2',
+				buttonsStyling: false
+			}).then(function () {
+				var url = 'http://forums.anodyne.dev:8888/admin/topics/' + topic + '/restore'
+				console.log(url)
+				
+				axios.put(url).then(function (response) {
+					swal({
+						title: "Topic was restored",
+						type: "success",
+						confirmButtonClass: 'btn btn-secondary',
+						buttonsStyling: false
+					})
+				}).catch(function (error) {
+					swal({
+						title: "Error!",
+						text: error,
+						type: "error",
+						confirmButtonClass: 'btn btn-outline-danger',
+						buttonsStyling: false
+					})
+				})
+			}, function (dismiss) {
+				if (dismiss === 'cancel') {
+					swal({
+						title: "Topic was not restored",
+						type: "error",
+						confirmButtonClass: 'btn btn-secondary',
+						buttonsStyling: false
+					})
+				}
+			})
+		})
+	</script>
 @endsection
