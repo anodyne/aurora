7d7f88349231c3e0c550f2755716dfd19a414b50
David VanScott <david.vanscott@gmail.com>
2017-05-04 08:17:16 -0400

    Make sure users can edit their signature

--- app/Data/User.php
+++ app/Data/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 	use Authorizable, Authorization, Notifiable, PresentableTrait;
 
 	protected $connection = 'users';
-	protected $fillable = ['points'];
+	protected $fillable = ['points', 'signature'];
 	protected $hidden = ['password', 'remember_token'];
 	protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 	protected $presenter = UserPresenter::class;
