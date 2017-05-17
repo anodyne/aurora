84e962a98f65348782da7fc7f406101c0d83aa64
David VanScott <david.vanscott@gmail.com>
2017-05-16 23:54:43 -0400

    Fix error with SVGs

--- config/blade-svg.php
+++ config/blade-svg.php
@@ -2,7 +2,7 @@
 
 return [
     'icon_path' => 'resources/assets/svg',
-    'spritesheet_path' => 'public/svg/symbol-defs.svg',
+    'spritesheet_path' => 'public/symbol-defs.svg',
     'inline' => true,
     'class' => 'icon',
 ];
