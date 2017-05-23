44b25a511a87beaa28ebb0dc8b1eb183ca35ee10
David VanScott <david.vanscott@gmail.com>
2017-05-23 14:41:28 -0400

    Update the paginator component

--- resources/assets/js/components/Paginator.vue
+++ resources/assets/js/components/Paginator.vue
@@ -1,29 +1,35 @@
 <template>
-	<ul class="pagination" v-if="shouldPaginate">
-		<li class="page-item" v-show="previousPageUrl">
-			<a class="page-link" href="#" aria-label="Previous" rel="previous" @click.prevent="page--">
-				<span aria-hidden="true">
-					<svg class="icon">
-						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-left"></use>
-					</svg>
-				</span>
-				<span class="sr-only">Previous</span>
-			</a>
-		</li>
-		<li class="page-item"><a class="page-link" href="#">1</a></li>
-		<li class="page-item"><a class="page-link" href="#">2</a></li>
-		<li class="page-item"><a class="page-link" href="#">3</a></li>
-		<li class="page-item" v-show="nextPageUrl">
-			<a class="page-link" href="#" aria-label="Next" rel="next" @click.prevent="page++">
-				<span aria-hidden="true">
-					<svg class="icon">
-						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
-					</svg>
-				</span>
-				<span class="sr-only">Next</span>
-			</a>
-		</li>
-	</ul>
+	<div>
+		<div class="discussion-offset">
+			<ul class="pagination" v-if="shouldPaginate">
+				<li class="page-item" v-show="previousPageUrl">
+					<a class="page-link" href="#" aria-label="Previous" rel="previous" @click.prevent="page--">
+						<span aria-hidden="true">
+							<svg class="icon">
+								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-left"></use>
+							</svg>
+						</span>
+						<span class="sr-only">Previous</span>
+					</a>
+				</li>
+
+				<li :class="pageItemClass(pageNum)" v-for="pageNum in this.dataSet.last_page">
+					<a class="page-link" href="#" @click.prevent="page = pageNum">{{ pageNum }}</a>
+				</li>
+
+				<li class="page-item" v-show="nextPageUrl">
+					<a class="page-link" href="#" aria-label="Next" rel="next" @click.prevent="page++">
+						<span aria-hidden="true">
+							<svg class="icon">
+								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+							</svg>
+						</span>
+						<span class="sr-only">Next</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -47,6 +53,14 @@
 		methods: {
 			broadcast () {
 				return this.$emit('changed', this.page)
+			},
+
+			pageItemClass (page) {
+				if (page == this.page) {
+					return 'page-item active'
+				}
+
+				return 'page-item'
 			},
 
 			updateUrl () {
