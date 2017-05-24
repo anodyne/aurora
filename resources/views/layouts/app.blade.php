f6b456ecd3563766f182ea75b89439f5b1361da0
David VanScott <david.vanscott@gmail.com>
2017-05-24 08:27:50 -0400

    Update the site icons and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 
 		<title>{{ config('app.name', 'Laravel') }}</title>
-		<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico?v1') }}">
+		<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico?v2') }}">
 		<link rel="apple-touch-icon-precomposed" href="{{ asset('apple-touch-icon.png') }}">
 
 		<!-- Scripts -->
