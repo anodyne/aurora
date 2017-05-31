11fb4ffb5a079a24a4a36b8d1d1b1300b0b3937f
David VanScott <david.vanscott@gmail.com>
2017-05-31 11:14:06 -0400

    Remove the repo binding method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -59,23 +59,6 @@ class AppServiceProvider extends ServiceProvider
 		});
 	}
 
-	protected function repositoryBindings()
-	{
-		$app = $this->app;
-
-		// Build a list of repositories that should be built
-		collect(['Discussion', 'Reply'])->each(function ($binding) use (&$app) {
-			// Set the concrete and abstract names
-			$abstract = "{$binding}RepositoryContract";
-			$abstractFQN = alias($abstract);
-			$concrete = alias("{$binding}Repository");
-
-			// Bind to the container and set the alias
-			$app->bind($abstractFQN, $concrete);
-			$app->alias($abstractFQN, $abstract);
-		});
-	}
-
 	protected function setModelObservers()
 	{
 		Data\Reply::observe(Observers\ReplyObserver::class);
