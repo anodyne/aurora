4c2862649e347065c157d5a92de5b1197478d7fe
David VanScott <david.vanscott@gmail.com>
2017-06-02 16:37:54 -0400

    Clean up the routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Auth::routes();
 
 Route::resource('discussions', 'DiscussionsController', ['except' => ['index']]);
@@ -38,14 +27,6 @@ Route::get('user/{user}', 'ProfilesController@show')->name('profile');
 Route::get('user/{user}/notifications', 'UserNotificationsController@index')->name('notifications.index');
 Route::delete('user/{user}/notifications/{notification}', 'UserNotificationsController@destroy')
 	->name('notifications.destroy');
-
-Route::get('login', function () {
-	//
-})->name('auth.login');
-
-Route::get('login', function () {
-	//
-})->name('login');
 
 Route::group(['prefix' => 'admin'], function () {
 	app('router')->bind('deletedTopic', function ($value) {
