d159ce6de208d1ad69aa9b022b5bb4fddf20d2ee
David VanScott <david.vanscott@gmail.com>
2017-06-05 17:09:41 -0400

    Clean up the routes file

--- routes/web.php
+++ routes/web.php
@@ -22,32 +22,40 @@ Route::delete('discussions/{topic}/{discussion}/subscriptions', 'DiscussionSubsc
 Route::post('replies/{reply}/favorites', 'FavoritesController@store')->name('favorites.store');
 Route::delete('replies/{reply}/favorites', 'FavoritesController@destroy')->name('favorites.destroy');
 
+/**
+ * Users
+ */
 Route::get('user/{user}', 'ProfilesController@show')->name('profile');
-
 Route::get('user/{user}/notifications', 'UserNotificationsController@index')->name('notifications.index');
+Route::delete('user/{user}/notifications', 'UserNotificationsController@destroyAll');
 Route::delete('user/{user}/notifications/{notification}', 'UserNotificationsController@destroy')
 	->name('notifications.destroy');
 
 Route::group(['prefix' => 'admin'], function () {
+	/**
+	 * Topic Management
+	 */
+	Route::resource('topics', 'TopicsController', ['except' => ['show']]);
+
+	// Make sure for restoring we can get the actual object
 	app('router')->bind('deletedTopic', function ($value) {
-		return App\Data\Topic::withTrashed()->where('slug', $value)->first();
+		return App\Data\Topic::withTrashed()->slug($value)->first();
 	});
 
-	Route::get('topics', 'TopicsController@index')->name('topics.index');
-	Route::get('topics/create', 'TopicsController@create')->name('topics.create');
-	Route::get('topics/{topic}/edit', 'TopicsController@edit')->name('topics.edit');
-	Route::get('topics/{topic}/remove', 'TopicsController@remove')->name('topics.remove');
-	Route::put('topics/{topic}', 'TopicsController@update')->name('topics.update');
+	// Add a route for restoring deleted topics
 	Route::put('topics/{deletedTopic}/restore', 'TopicsController@restore')
 		->name('topics.restore');
-	Route::post('topics', 'TopicsController@store')->name('topics.store');
-	Route::delete('topics/{topic}', 'TopicsController@destroy')->name('topics.destroy');
+
+	/**
+	 * Announcement management
+	 */
+	Route::resource('announcements', 'AnnouncementsController', ['except' => ['show']]);
 });
 
 Route::get('topics/{topic}', 'DiscussionsController@index')->name('topics.discussions');
 
 Route::get('test', function () {
-	auth()->setUser(User::find(85));
+	auth()->setUser(User::find(1));
 	view()->share('_user', auth()->user());
 
 	return view('pages.test');
