ea87e31c67de7ee4139706139c192285a3814ad4
David VanScott <david.vanscott@gmail.com>
2017-06-08 16:38:28 -0400

    Don't compile something that's not there

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,4 @@ const { mix } = require('laravel-mix')
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/responsive.scss', 'public/css')
-   .sass('resources/assets/sass/errors.scss', 'public/css')
-   .styles([
-   		'node_modules/medium-editor/dist/css/medium-editor.min.css',
-   		'node_modules/medium-editor/dist/css/themes/beagle.min.css'
-   	], 'public/css/medium-editor.css');
+   .sass('resources/assets/sass/errors.scss', 'public/css');
