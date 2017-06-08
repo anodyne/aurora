ea24fc29a1c7897c9e0e1f4320d30b24e7aa8404
David VanScott <david.vanscott@gmail.com>
2017-06-08 09:55:42 -0400

    Work on creating a discussion

---
+++ resources/assets/js/pages/CreateDiscussion.vue
@@ -0,0 +1,34 @@
+<script>
+	import Quill from 'quill';
+
+	export default {
+		data () {
+			return {
+				body: '',
+				editor: false
+			}
+		},
+
+		mounted () {
+			let self = this;
+
+			this.editor = new Quill('#editor', {
+				modules: {
+					toolbar: [
+						['bold', 'italic', 'underline'],
+						['blockquote', 'code-block'],
+						['link', 'image'],
+						[{ 'header': [2, 3, false] }],
+						[{ 'list': 'ordered'}, { 'list': 'bullet' }]
+					]
+				},
+				placeholder: "Let us know what's on your mind...",
+				theme: 'bubble'
+			});
+
+			this.editor.on('text-change', function (delta) {
+				self.body = self.editor.container.firstChild.innerHTML;
+			});
+		}
+	}
+</script>
--- resources/views/pages/discussions/create.blade.php
+++ resources/views/pages/discussions/create.blade.php
@@ -3,41 +3,48 @@
 @section('content')
 	<h1>Start a Discussion</h1>
 
-	{!! Form::open(['route' => 'discussions.store']) !!}
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label>Pick a Topic</label>
-					{!! Form::topics() !!}
+	<create-discussion inline-template>
+		{!! Form::open(['route' => 'discussions.store']) !!}
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="sr-only">Pick a Topic</label>
+						{!! Form::topics() !!}
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label>Title</label>
-					{!! Form::text('title', null, ['class' => 'form-control']) !!}
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label class="sr-only">Title</label>
+						{!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Discussion title']) !!}
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-md-8">
-				<div class="form-group">
-					<label class="control-label">Content</label>
-					{!! Form::textarea('body', null, ['class' => 'form-control', 'rows' => 8]) !!}
+			<div class="row">
+				<div class="col-md-8">
+					<div class="form-group">
+						<label class="sr-only">Content</label>
+						<div id="editor"></div>
+						<textarea name="body" class="form-control hidden" v-model="body"></textarea>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			{!! Form::button("Start Discussion", ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
-			<a href="{{ route('home') }}" class="btn btn-link-secondary ml-2">Cancel</a>
-		</div>
-	{!! Form::close() !!}
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Start Discussion</button>
+				<a href="{{ route('home') }}" class="btn btn-link-secondary ml-2">Cancel</a>
+			</div>
+		{!! Form::close() !!}
+	</create-discussion>
 @endsection
 
-@push('styles')
-	<link href="{{ asset('css/quill.css') }}" rel="stylesheet">
-@endpush
+@section('js')
+	<script>
+		var vue = new Vue({
+			el: "#app"
+		});
+	</script>
+@endsection
